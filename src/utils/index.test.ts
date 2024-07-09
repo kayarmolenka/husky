a1c56c1f303d9819c13e755262673eb353bd9e5c
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2024-07-09 16:11:17 +0300

    fix test

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,7 +1,7 @@
 import { summ } from './index';
 
 describe('Utils tests', () => {
-    it('should summ', () => {
+    it('should return summ result', () => {
        const result =  summ(5,6)
 
         expect(result).toBe(11)
